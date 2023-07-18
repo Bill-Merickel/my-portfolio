f50a0087e9d416febd7ebd58daecf7871a42daad
Bill Merickel <billmerickel@gmail.com>
2023-07-17 21:43:29 -0400

    Update gatsby config website info

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'Bill Merickel',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Bill Merickel is a computer science student from Georgia Tech pursuing a career in software engineering.',
+    siteUrl: 'https://billmerickel.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@billmerickel',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
